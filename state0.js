358faa3557c239a5f376b1e0a033059b18139ea7
pablo <hortonwhy@protonmail.com>
2021-02-26 16:48:52 -0600

    draws blackscreen now

--- state0.js
+++ state0.js
@@ -1,1 +1,21 @@
-#state0
+// State0 will be the titlescreen
+var game = {};
+
+// define all the states here
+slime.state0 = function() {};
+slime.state0.prototype = {
+  preload: function() {
+
+  },
+
+  create: function() {
+    game.stage.backgorundcolor = "#dddddd";
+
+  },
+
+  update: function() {
+
+  },
+}
+
+
